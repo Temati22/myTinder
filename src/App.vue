8f47136ebc65e980416bb3d39d53927c74aefaf1
artem <temati22@gmail.com>
2021-03-16 02:36:44 +0300

    My tinder - first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,213 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="title">
+            It`s my TINDER
+        </div>
+
+        <div class="wrapper-card">
+
+            <item v-for="item in itemsList" :key="item.index" :img="item.src.medium"
+                  :description="item.photographer_url"
+                  :name="item.photographer" @touchmove="getTouchmove(event)"/>
+
+            <!--SKELETON PREVIEW-->
+            <div v-if="itemsList.length == 0">
+                <figure class="card-item">
+                    <div class="image-wrapper">
+                        <Skeleton width="350px" height="400px"></Skeleton>
+                    </div>
+                </figure>
+            </div>
+
+
+            <div class="block-buttons">
+                <button class="btn btn-red dislike" @click="dislike"></button>
+                <button class="btn btn-green like" @click="like"></button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import {Skeleton} from 'vue-loading-skeleton'; // немного красоты, в те моменты, пока нету постов
+    import item from "@/components/item"; // компонент поста, занимается только выводом
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    // сторонняя апи, которая отдает картинки
+    import {createClient} from 'pexels';
+
+    const client = createClient('563492ad6f917000010000012e26aad4b40241feb2d8f6bfbe3522f3');
+    const query = 'girl';
+    // это все к ней же
+
+
+    export default {
+        name: 'App',
+        data: function () {
+            return {
+                itemsList: [],
+                page: 1
+            }
+        },
+        components: {
+            item,
+            Skeleton
+        },
+        created() {
+            this.getMoreItems();
+        },
+        mounted(){
+
+            // find the element that you want to drag.
+            let box = this.$el.querySelectorAll('.wrapper-card .card-item');
+
+            console.log(box);
+            /* listen to the touchmove event,
+            every time it fires, grab the location
+            of touch and assign it to box */
+
+            // box.addEventListener('touchmove', function(e) {
+            //     var touchLocation = e.targetTouches[0];
+            //     box.style.left = touchLocation.pageX + 'px';
+            //     box.style.top = touchLocation.pageY + 'px';
+            // });
+        },
+        methods: {
+
+            like: function () {
+                let $this = this;
+                let elem = this.$el.querySelectorAll('.wrapper-card .card-item');
+                elem[this.itemsList.length - 1].classList.add('like');
+
+                setTimeout(function () {
+                    $this.itemsList.pop(); // удаляем элемент из массива, можно сохранить в переменную, так как pop вернет нам удаленный элемент
+
+                    /* Тут можем делать все что угодно с нашим ответом, так как мы знаем какую карточку удалили и что ответил нам пользователь  */
+                    /* Мысль была сохранить лайки и дизлайки с id карточками и всей пачкой посылать на сервер, либо при каждом ответе, что поможет нам не потерять ответ пользователя*/
+
+                }, 300);
+
+                // подгружаем новых людей, если они закончились в стэке
+                if ($this.itemsList.length - 1 == 0) {
+                    $this.getMoreItems();
+                }
+
+            },
+
+            dislike: function () {
+                let $this = this;
+                let elem = this.$el.querySelectorAll('.wrapper-card .card-item');
+
+                // добавляем класс для анимации действия
+                elem[this.itemsList.length - 1].classList.add('dislike');
+
+                setTimeout(function () {
+                    $this.itemsList.pop(); // удаляем элемент из массива, можно сохранить в переменную, так как pop вернет нам удаленный элемент
+                    /* Тут можем делать все что угодно с нашим ответом, так как мы знаем какую карточку удалили и что ответил нам пользователь  */
+                    /* Мысль была сохранить лайки и дизлайки с id карточками и всей пачкой посылать на сервер, либо при каждом ответе, что поможет нам не потерять ответ пользователя*/
+
+                }, 300);
+
+                // подгружаем новых людей, если они закончились в стэке
+                if ($this.itemsList.length - 1 == 0) {
+                    $this.getMoreItems();
+                }
+            },
+
+            getMoreItems: function () {
+                let $this = this;
+
+                client.photos.search({query, per_page: 10, page: $this.page}).then(photos => {
+                    photos.photos.forEach((el) => {
+                        $this.itemsList.push(el);
+                    });
+                    $this.page++;
+                });
+            },
+            getTouchmove(event){
+                // let $this = this;
+                console.log('touchmove');
+                console.log(event);
+                // var touchLocation = e.targetTouches[0];
+                // box.style.left = touchLocation.pageX + 'px';
+                // box.style.top = touchLocation.pageY + 'px';
+            }
+
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    @import "/src/assets/scss/reset.scss";
+
+
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+
+        .title {
+            padding: 20px 0;
+            text-align: center;
+            font-size: 24px;
+        }
+
+
+        .wrapper-card {
+            max-width: 350px;
+            margin: 0 auto;
+            position: relative;
+            min-height: 550px;
+        }
+
+        .block-buttons {
+            width: 100%;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            flex-wrap: wrap;
+            margin: 20px auto;
+            position: absolute;
+            bottom: 0;
+
+            .btn {
+                background: none;
+                padding: 10px 20px;
+                border-radius: 10px;
+                height: 50px;
+                width: 120px;
+                margin: 0 5px;
+                cursor: pointer;
+
+                &-red {
+                    color: #fff;
+                    background: #f0f0f0 url("../src/assets/img/dis.png") no-repeat center center;
+                    background-size: 30px;
+                    border: 1px solid #f0f0f0;
+
+                    &:hover {
+                        background: #000 url("../src/assets/img/dis.png") no-repeat center center;
+                        background-size: 30px;
+                    }
+                }
+
+                &-green {
+                    color: #fff;
+                    background: #f0f0f0 url("../src/assets/img/like2.png") no-repeat center center;
+                    background-size: 30px;
+                    border: 1px solid #f0f0f0;
+
+
+                    &:hover {
+                        background: #000 url("../src/assets/img/like2.png") no-repeat center center;
+                        background-size: 30px;
+                    }
+                }
+
+
+            }
+        }
+    }
+
 </style>
---
+++ src/components/item.vue
@@ -0,0 +1,154 @@
+<template>
+    <figure class="card-item">
+        <div class="image-wrapper">
+            <img :src="img" alt="">
+        </div>
+
+        <div class="card-item__descr">
+            <div class="card-item__descr-title">
+                {{name}}
+            </div>
+            <div class="card-item__descr-description">
+                {{description}}
+            </div>
+            <div class="card-item__descr-tags-like">
+                <ul>
+                    <li>
+                        Songs
+                    </li>
+                    <li>
+                        Art
+                    </li>
+                    <li>
+                        Sex
+                    </li>
+                    <li>
+                        Buisenes
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </figure>
+</template>
+
+<script>
+
+    export default {
+        name: 'Item',
+        props: [
+            'img',
+            'name',
+            'description'
+        ]
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+    ul {
+        list-style: none;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        flex-wrap: wrap;
+
+        li {
+            padding: 5px 10px;
+            border-radius: 6px;
+            border: 1px solid #f0f0f0;
+            margin-right: 10px;
+            background: #fff;
+        }
+    }
+
+
+    .card-item {
+        height: 450px;
+        border-radius: 10px;
+        overflow: hidden;
+        box-shadow: 7px 7px 10px #bababa;
+        transition: all ease-in-out .3s;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        background: #fff;
+        transform-origin: 50% 100%;
+        transition: all ease-in-out .3s;
+
+        &.like {
+            transform: rotate3d(1, 1, 3, 40deg);
+            animation-duration: .3s;
+            animation-name: fadeout;
+        }
+
+        &.dislike {
+            transform: rotate3d(1, 1, 3, -40deg);
+            animation-duration: .3s;
+            animation-name: fadeout;
+        }
+
+        .image-wrapper {
+            overflow: hidden;
+            height: 450px;
+            position: relative;
+
+            img {
+                min-width: 100%;
+                transform: translateX(-50%);
+                position: absolute;
+                left: 50%;
+                top: 0;
+                bottom: 0;
+                min-height: 100%;
+            }
+        }
+
+
+        &__descr {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            padding: 20px;
+
+            div {
+                line-height: 1.2;
+            }
+
+            &-title {
+                font-size: 34px;
+                font-weight: bold;
+                text-align: left;
+                text-shadow: 0 0 5px #000;
+                color: #fff;
+            }
+
+            &-description {
+                font-size: 18px;
+                text-align: left;
+                padding: 10px 0;
+                color: #fff;
+            }
+        }
+
+
+    }
+
+
+    @keyframes fadeout {
+        from {
+            opacity: 1;
+        }
+
+        to {
+            opacity: 0;
+        }
+
+        75% {
+            opacity: 0.75;
+        }
+    }
+
+
+</style>
